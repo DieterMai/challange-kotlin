2e43b03d17cbb403c928cba61b9f321e2d6d960a
Dieter Mai <maidieter@googlemail.com>
2023-06-09 14:13:26 +0200

    Add Kata Multiply

---
+++ InProgress/k15.cw.kata8.Multiply/src/k15/cw/kata8/multiply/Main.kt
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004
+// Multiply
+
+package k15.cw.kata8.multiply
+
+fun multiply(x: Double, y: Double) : Double
+{
+    return x * y
+}
---
+++ InProgress/k15.cw.kata8.Multiply/test/k15/cw/kata8/multiply/TestExample.kt
@@ -0,0 +1,24 @@
+package k15.cw.kata8.multiply
+
+import k15.cw.kata8.multiply.multiply
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+
+internal class TestExample {
+    @Test
+    @Throws(Exception::class)
+    fun test_Associativity_Of_Multiplication() {
+        for (i in 0..99) {
+            val a = Math.random()
+            val b = Math.random()
+            val c = Math.random()
+            val message = String.format("(%g * %g) * %g == %g * (%g * %g)", a, b, c, a, b, c)
+            assertEquals(message,
+                multiply(multiply(a, b), c),
+                multiply(a, multiply(b, c)),
+                1E-14)
+            assertEquals(multiply(a, b), a * b, 1e-3);
+        }
+    }
+}
